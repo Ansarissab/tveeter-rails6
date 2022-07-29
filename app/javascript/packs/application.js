1270833a5e434a914a36a7eb4e86b7d087f7cbbb
Ansarissab <zahidensari116@gmail.com>
2022-07-29 15:19:30 +0500

    Fixed bootstrap issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
+import "../controllers"
+import "@hotwired/turbo-rails"
+// Rails.start()
+// Turbolinks.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
